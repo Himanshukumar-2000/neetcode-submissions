0419eb4042341ab254fac5d3fc80ad1f3b50bc2b
Himanshukumar-2000 <himanshukumar20apr@gmail.com>
2026-06-05 23:56:41 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==1) return nums[0];
+
+        sort(nums.begin(), nums.end());
+        
+        int max_element = nums[0];
+        int max_count = 1;
+        int current_count = 1;
+
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
+            current_count++;
+        } else {
+            current_count = 1;
+        }
+        if(current_count > max_count){
+            max_count = current_count;
+            max_element = nums[i];
+        }
+        }
+        return max_element;
+
+        /*
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+        */
+    }
+};
